113db1e6f9f820ceb54eb44f29b1aa09dd7d7e32
arunkindo2000-eng <arunkindo2000@gmail.com>
2026-03-14 23:50:16 +0530

    Integrate Firebase for chat functionality

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-document.querySelector("button").onclick = () => {
-  alert("Connecting to TalkToSmile Server...");
-};
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
+
+const db = getDatabase();
+
+let userRef = null;
+
+function startChat() {
+  const waitingRef = ref(db, "waiting");
+
+  userRef = push(waitingRef, {
+    status: "waiting"
+  });
+
+  console.log("Waiting for stranger...");
+}
+
+function disconnectChat() {
+  if (userRef) {
+    remove(userRef);
+    userRef = null;
+  }
+  console.log("Disconnected");
+}
+
+document.querySelector("#startChat").onclick = startChat;
+document.querySelector("#disconnect").onclick = disconnectChat;
